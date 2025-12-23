6c3ef5873f9e2df678c4cdbc51441fc3cc1e5c31
lamoanky <liuoliver08@gmail.com>
2025-12-22 19:35:48 -0700

    made this more friendly with lots of games

--- readFile.py
+++ readFile.py
@@ -10,27 +10,30 @@ pgn = open("dataset/lichess_elite_2020-06.pgn")
 positions = []
 moves = []
 currentGame = 0
-maxGames = 100
+maxGames = 1000
+quarterGames = maxGames // 4
 print("Initializing game...")
 
 for i in range(maxGames):
     game = chess.pgn.read_game(pgn)
     board = game.board()
+
     for move in game.mainline_moves():
         boardArray = boardToTensor(board)
         moveValue = moveToValue(move.uci())
 
-        
-
         positions.append(boardArray)
         moves.append(moveValue)
         board.push(move)
+    if (i+1) % quarterGames == 0:
+        print(f"Read {((i+1) / maxGames) * 100}% of total games.")
 
+print("Converting to tensors...")
 boardTensor = torch.FloatTensor(numpy.array(positions))
 moveTensor = torch.LongTensor(numpy.array(moves))
-
+print("Done converting to tensors!")
 
 dataset = ChessPositionDataset(boardTensor, moveTensor)
-print(f"Done reading game! Read a total of: {len(dataset)} positions.")
+print(f"Done reading games! Read a total of: {len(dataset)} positions.")
 
 pgn.close()
